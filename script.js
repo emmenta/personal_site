d7fb536140698cdf3dd87bf5d087fdb0e8923587
Emmenta Janneh <142241685+emmenta@users.noreply.github.com>
2025-05-26 17:44:07 -0400

    Add files via upload
    Initial

---
+++ script.js
@@ -0,0 +1,7 @@
+function showTab(tabName) {
+    document.getElementById('about').style.display = 'none';
+    document.getElementById('projects').style.display = 'none';
+    document.getElementById('certificates').style.display = 'none';
+    document.getElementById(tabName).style.display = 'block';
+  }
+  
